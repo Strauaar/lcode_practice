9556a6b16c6a94e1046168b197f27271e34bbec4
Aaron Strauli <astrauli@gmail.com>
2018-02-07 22:56:13 -0800

    complete

--- partition_list.rb
+++ partition_list.rb
@@ -11,5 +11,25 @@ end
 # @param {Integer} x
 # @return {ListNode}
 def partition(head, x)
-    
+    pre = []
+    post = []
+    current_node = head
+    until current_node == nil
+        if current_node.val < x
+            pre << current_node.val
+        else
+            post << current_node.val
+        end
+        current_node = current_node.next
+    end
+    correct_order = pre + post
+    ll = []
+    for i in 0...correct_order.length
+        node = ListNode.new(correct_order[i]) 
+        ll[i] = node
+    end
+    for i in 0...ll.length - 1
+        ll[i].next = ll[i + 1] 
+    end
+    ll[0]
 end
